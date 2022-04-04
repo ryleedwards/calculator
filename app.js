761fd225658bb5c90eeac7c936a32725edb75a4c
Ryle Edwards <ryle.edwards@gmail.com>
2022-04-03 21:33:16 -0500

    Add logic for operator and math functions

---
+++ app.js
@@ -0,0 +1,34 @@
+/* Register buttons */
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target);
+  });
+});
+
+/* Operator function to call add, subtract, multiply, divide */
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "x":
+      return multiply(num1, num2);
+    case "÷":
+      return divide(num1, num2);
+  }
+}
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+const subtract = (num1, num2) => {
+  return num1 - num2;
+};
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+const divide = (num1, num2) => {
+  return num1 / num2;
+};
