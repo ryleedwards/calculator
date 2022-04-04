099c859df5abe2761741fe22aa5e726f5b3fcf8a
Ryle Edwards <ryle.edwards@gmail.com>
2022-04-04 15:38:46 -0500

    Implement math functions in working state

--- app.js
+++ app.js
@@ -3,37 +3,56 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     let input = e.target.classList[1];
-    switch (input) {
-      case "digit":
-        addToDisplay(e.target);
-        break;
-      case "clear":
-        clear();
-        break;
-      case "add":
-      case "subtract":
-      case "multiply":
-      case "divide":
-        let oldValue = currentValue;
-        currentValue = storeValue();
-        console.log(`oldValue: ${oldValue} // currentValue: ${currentValue}`);
-        if (oldValue) {
-          let solution = operate(input, oldValue, currentValue);
-          console.log(`solution: ${solution}`);
-          display.textContent = solution.toString();
-          currentValue = solution;
-        }
-        break;
-      case "equal":
-    }
+    checkInput(e, input);
   });
 });
 
 /* Register display */
 const display = document.querySelector(".output");
+const topDisplay = document.querySelector(".top-output");
 
-/* Declare value for displaying */
-let currentValue;
+/* Declare values to be stored 
+value1 = bottom value
+value2 = top value */
+let value1, value2, tempValue;
+prevOperator = undefined;
+
+function checkInput(e, input) {
+  let inputSymbol = "";
+  switch (input) {
+    case "digit":
+      addToDisplay(e.target);
+      break;
+    case "clear":
+      fullClear();
+      break;
+    case "add":
+    case "subtract":
+    case "multiply":
+    case "divide":
+      if (value1 == undefined) {
+        value1 = parseFloat(display.textContent);
+        console.log(`stored value1 as ${value1}`);
+        topDisplay.textContent = `${value1}`;
+        display.textContent = "0";
+        prevOperator = input;
+        console.log(`prevOperator stored as ${prevOperator}`);
+      } else if (value1) {
+        value2 = parseFloat(display.textContent);
+        console.log(`stored value2 as ${value2}`);
+        display.textContent = "0";
+      }
+      break;
+    case "equal":
+      value2 = parseFloat(display.textContent);
+      console.log(`v1 = ${value1} // v2 = ${value2} // tV = ${tempValue}`);
+      tempValue = operate(prevOperator, value1, value2);
+      topDisplay.textContent = "";
+      display.textContent = tempValue;
+      value1 = undefined;
+      break;
+  }
+}
 
 /* Operator function to call add, subtract, multiply, divide */
 function operate(operator, num1, num2) {
@@ -73,11 +92,9 @@ function addToDisplay(digitButton) {
   }
 }
 
-function storeValue() {
-  return parseFloat(display.textContent);
-}
-
-function clear() {
+function fullClear() {
   display.textContent = "0";
-  currentValue = undefined;
+  topDisplay.textContent = "";
+  value1 = undefined;
+  value2 = undefined;
 }
